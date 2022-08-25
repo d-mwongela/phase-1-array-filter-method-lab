222983339160aff6a2a1a8cba683c68a67a6ade5
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-25 11:10:13 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+/*
+
+function findMatching(drivers, name){
+    return name.filter(
+        (matchingNames) => matchingNames.toLowerCase() === drivers.toUpperCase()
+ }
+ */
+
+function findMatching(drivers, name) {
+    return drivers.filter((drivers) => name.toLowerCase() === drivers.toLowerCase());
+}
+
+function fuzzyMatch(drivers, name) {
+return drivers.filter((driver) => driver.startsWith(name, 0));
+}
+
+function matchName (person, propertiesByName) {
+    return person.filter(
+        details => details.name === propertiesByName
+    )
+}
